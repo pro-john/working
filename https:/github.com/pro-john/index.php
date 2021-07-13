4569e7acace9f2655ff7039cc825fc9219e5d4fd
pro-john <46971302+pro-john@users.noreply.github.com>
2021-07-13 16:54:19 +0300

    Add files via upload

---
+++ https:/github.com/pro-john/index.php
@@ -0,0 +1,15 @@
+<?php
+ 
+$client  = @$_SERVER['HTTP_CLIENT_IP'];
+$forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+$remote  = @$_SERVER['REMOTE_ADDR'];
+ 
+if(filter_var($client, FILTER_VALIDATE_IP)) $ip = $client;
+elseif(filter_var($forward, FILTER_VALIDATE_IP)) $ip = $forward;
+else $ip = $remote;
+ 
+echo $ip;
+
+echo date(": d:m:Y--H:i:s"); 
+
+?>
